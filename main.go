aa26f7848f3f830aa6c3e2c11baf3f9d7772ba5a
Dan Harasymiw <danharasymiw@gmail.com>
2017-08-05 00:45:27 -0400

    fix

--- main.go
+++ main.go
@@ -2,79 +2,137 @@ package main
 
 import (
 	"fmt"
-	"net/url"
-	"github.com/Philipp15b/go-steamapi"
+	"strings"
+	"github.com/danharasymiw/disteam/steam"
+	"os"
 )
 
 
-
 func main() {
-	key := "API_KEY_HERE"
-	var names = []string{"test"}
+	key := os.Getenv("STEAM_KEY")
 
-	findCommonSteamGames(names, key);
+	if key == "" {
+		fmt.Println("STEAM_KEY ENV VAR NOT SET")
+	}
+	fmt.Println(getCommonGames(os.Args[1:], key))
 }
 
-func findCommonSteamGames(usernames []string, key string) {
-	count := 0
-	var games [1][]string;
+
+func getCommonGames(usernames []string, key string) string {
+
+	steamIds := make([]string, 0, len(usernames))
+	unfoundUsers := make(map[string] bool)
+
+	// try and find people
+
 	for _, username := range usernames {
 		steamId, err := usernameToSteamId(username, key)
 
-		if err != nil {
-			count = count + 1
-			games[count] = getOwnedGames(steamId, key)
-			print (games[count])
+		if err == nil && steamId != "" {
+			steamIds = append(steamIds, steamId)
+		} else {
+			unfoundUsers[username] = true
 		}
 
+		for unfoundUser, _:= range unfoundUsers {
+			for _, steamId := range steamIds {
+				foundSteamId := checkFriendsForUser(steamId, unfoundUser)
+
+				if foundSteamId != "" {
+					steamIds = append(steamIds, foundSteamId)
+					delete(unfoundUsers, unfoundUser)
+				}
+			}
+		}
+
+		if len(unfoundUsers) > 0 {
+			output := "Unable to find: "
+			for user, _ := range unfoundUsers {
+				output += user + ", "
+			}
+			fmt.Println(output[:len(output) - 2])
+		}
 	}
 
+	allGames := getAllUsersGames(steamIds, key)
+	commonGames := findIntersection(allGames)
+	gamesString := strings.Join(commonGames, ", ")
 
+	return fmt.Sprintf("There are %d games in common: %s", len(commonGames), gamesString)
 }
 
-func getOwnedGames(steamId, key string) ([]string, error){
-	var getGames = steamapi.NewSteamMethod("IPlayerService", "GetOwnedGames", 1)
-
-	data := url.Values{}
-	data.Add("key", key)
-	data.Add("steamid", steamId)
-	data.Add("include_appInfo", "1")
-
-	var resp GetOwnedGamesResponse
-	err := getGames.Request(data, &resp)
-	if err != nil {
-		return nil, err
-	}
-
-	var gameNames [resp.Game_count]string
-	for index, name := range resp.Games {
-		gameNames[index] = name
-	}
-
-	return gameNames, nil
+func checkFriendsForUser(steamId string, unfoundName string) (string) {
+	// get friends
+	// check each friends persona name
+	// if we find a match return the steam id, else 0
+	return ""
 }
 
-func usernameToSteamId(username, key string) (steamapi.ResolveVanityURLResponse, error) {
-	response, err := steamapi.ResolveVanityURL(username, key)
+func findIntersection(allGames [][]string) []string {
+	var gamesMap = make(map[string]int)
+
+	for _, games := range allGames {
+		for _, game := range games {
+			if val, ok := gamesMap[game]; ok {
+				gamesMap[game] = val + 1
+			} else {
+				gamesMap[game] = 1
+			}
+		}
+	}
+
+	count := 0
+	var commonGames = make([]string, len(gamesMap))
+	for gameName, numOfOwned := range gamesMap {
+		if numOfOwned == len(allGames) {
+			commonGames[count] = gameName
+			count++
+		}
+	}
+
+	commonGames = commonGames[:count]
+
+	return commonGames
+}
+
+func getAllUsersGames(steamIds []string, key string) [][]string {
+
+	count := 0
+	allGames  := make([][]string, len(steamIds))
+
+	for _, steamId := range steamIds {
+		userGames, err := getOwnedGameNames(steamId, key)
+
+		if err != nil {
+			fmt.Printf("Couldn't find games for steam id: %s", steamId)
+			return nil
+		}
+
+		allGames[count] = userGames
+		count = count + 1
+	}
+
+	return allGames[:count]
+}
+
+func getOwnedGameNames(steamId string, key string) ([]string, error){
+	games, _ := steam.GetOwnedGames(steamId, key)
+
+	var names = make([]string, games.Count)
+	for index, game := range games.Games {
+	     names[index] = game.Name
+	}
+
+
+	return names, nil
+}
+
+func usernameToSteamId(username, key string) (string, error) {
+	response, err := steam.ResolveVanityUrl(username, key)
 
 	if err != nil || response.Success != 1 {
-		return nil, err
+		return "", err
 	}
 
-	return response.SteamID, nil
-}
-
-type GetOwnedGamesResponse struct {
-	Game_count int
-	Games []Game
-}
-
-type Game struct {
-	Success int
-	AppId int
-	Name string
-	Playtime_forever int
-	Img_icon_url string
-	Img_logo_url string
-	Has_community_visible_stats bool
+	return response.SteamID, err
 }
---
+++ steam/steam.go
@@ -0,0 +1,74 @@
+package steam
+
+import (
+	"net/http"
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+)
+
+type VanityUrlJson struct {
+	Response VanityUrlResponseJson `json:"response"`
+}
+
+type VanityUrlResponseJson struct {
+	Success int `json:"success"`
+	SteamID string `json:"steamid"`
+}
+
+func ResolveVanityUrl(username, key string) (VanityUrlResponseJson, error) {
+	url := fmt.Sprintf("https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%s&vanityurl=%s",
+		key, username)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	vanityUrl := new(VanityUrlJson)
+	json.Unmarshal(body, vanityUrl)
+
+	return vanityUrl.Response, nil
+}
+
+
+
+type GetOwnedGamesJson struct {
+	Response GetOwnedGamesResponseJson `json:"response"`
+}
+
+type GetOwnedGamesResponseJson struct {
+	Count int `json:"game_count"`
+	Games []GameJson `json:"games"`
+}
+
+type GameJson struct {
+	Name string `json:"name"`
+}
+
+func GetOwnedGames(steamId, key string) (GetOwnedGamesResponseJson, error) {
+	url := fmt.Sprintf("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&include_appinfo=1", key, steamId)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		panic(err2)
+	}
+	getOwnedGames := new(GetOwnedGamesJson)
+	json.Unmarshal(body, getOwnedGames)
+
+	return getOwnedGames.Response, nil
+}
